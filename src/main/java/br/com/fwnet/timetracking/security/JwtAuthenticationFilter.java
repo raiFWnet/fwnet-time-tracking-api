8674b1de912e62f59222ebefffd35da35269ae75
Raí Onório <raionorio0204@gmail.com>
2026-09-14 15:51:47 -0300

    fix: reject disabled user session

--- src/main/java/br/com/fwnet/timetracking/security/JwtAuthenticationFilter.java
+++ src/main/java/br/com/fwnet/timetracking/security/JwtAuthenticationFilter.java
@@ -57,7 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             UserDetails userDetails =
                     customUserDetailsService.loadUserByUsername(email);
 
-            if (jwtService.isTokenValid(token, userDetails)) {
+            if (userDetails.isEnabled() && jwtService.isTokenValid(token, userDetails)) {
 
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(
