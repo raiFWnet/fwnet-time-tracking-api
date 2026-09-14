e218ef026901b8a847def245233051fd0ca28740
Raí Onório <raionorio0204@gmail.com>
2026-09-14 16:59:05 -0300

    feat: add swagger openapi documentation

--- src/main/java/br/com/fwnet/timetracking/controller/UserController.java
+++ src/main/java/br/com/fwnet/timetracking/controller/UserController.java
@@ -3,6 +3,7 @@ package br.com.fwnet.timetracking.controller;
 import br.com.fwnet.timetracking.dto.request.CreateUserRequest;
 import br.com.fwnet.timetracking.dto.response.UserResponse;
 import br.com.fwnet.timetracking.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@SecurityRequirement(name = "bearerAuth")
 public class UserController {
 
     private final UserService userService;
